23e59461e76fe90cd2e49e1c059dd6142a425ef6
Mohzeba <baigmohzeba@gmail.com>
2026-03-07 00:03:54 +0530

    Grid 4 added

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,7 @@
 import Card from "../components/Card";
 import { useRef } from "react";
 import { Globe } from "../components/Globe";
+import CopyEmailButton from "../components/CopyEmailButton";
 
 const About = () => {
   const grid2container = useRef();
@@ -46,7 +47,13 @@ const About = () => {
           </figure>
         </div>
         {/* Grid 4 */}
-        <div className="grid-special-color grid-4"></div>
+        <div className="grid-special-color grid-4">
+          <div className="flex flex-col items-center justify-center gap-4 size-full">
+            <p className=" text-center headtext">Do you want to start a project together</p>
+            <CopyEmailButton />
+          </div>
+
+        </div>
         {/* Grid 5 */}  
         <div className="grid-default-color grid-5"></div>
       </div>
