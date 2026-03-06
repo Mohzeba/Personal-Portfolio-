270961de890c625616656ed8541f814a7e3c1d6b
Mohzeba <baigmohzeba@gmail.com>
2026-03-06 06:08:55 +0530

    about

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import {motion} from "motion/react";
 const Card = ({style, text, image, containerRef}) => {
   return image && !text?(
-    <img className="absolute w-15 cursor-grab" src={image} style={style}
+    <motion.img className="absolute w-15 cursor-grab" src={image} style={style}
     whileHover={{scale:1.05}}
     drag
     dragConstraints={containerRef}
